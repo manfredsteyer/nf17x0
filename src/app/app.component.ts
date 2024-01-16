0b58f38048a204b335de1d3574435e68e16c1ec7
Manfred Steyer <manfred.steyer@gmx.net>
2024-01-16 12:07:09 +0100

    use shared lib with path mapping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedLibComponent } from 'shared-lib';
+import { SharedLibService } from 'shared-lib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedLibComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'nf17x0';
+  service = inject(SharedLibService);
+  
+  constructor() {
+    console.log('service', this.service);
+  }
 }
